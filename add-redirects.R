547ab1a210da6decd3445b2500da73eb0dd49213
cynthiahqy <29718979+cynthiahqy@users.noreply.github.com>
2022-11-07 19:17:50 +1100

    separate R code for date-free slugs to _redirects
    See issue #1

---
+++ add-redirects.R
@@ -0,0 +1,16 @@
+# list names of gallery folders
+posts <- list.dirs(
+  path = here::here("gallery/sketchnotes"),
+  full.names = FALSE,
+  recursive = FALSE
+)
+
+# extract the slugs
+slugs <- gsub("^.*_", "", posts)
+
+# lines to insert to a netlify _redirect file
+redirects <- paste0("/", slugs, " ", "/gallery/sketchnotes/", posts)
+
+# write the _redirect file
+writeLines(redirects,
+           file.path(Sys.getenv("QUARTO_PROJECT_OUTPUT_DIR"), "_redirects"))
